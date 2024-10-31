ac34b60b20c450cac8c08e76d9a39d2557cfd735
Yann <yann.lemasson33@gmail.com>
2024-10-31 22:14:01 +0100

    Correction of StdOutput filename

--- src/Outputs/StdOutput.cpp
+++ src/Outputs/StdOutput.cpp
@@ -1,26 +1,22 @@
 #include "StdOutput.hpp"
+#include "Utilities.hpp"
 
 namespace phi
 {
     void StdOutput::Log(const std::string &inText, eLogLevel inLevel, const std::time_t &inDate) const {
-        if(inLevel >= mMinimumLevel) {
+    if(inLevel >= mMinimumLevel) {
+        
+        // Color
+        std::string begin_color = "";
+        std::string end_color = "";
 
-            // Date
-            std::tm* local_time = std::localtime(&inDate);
-            std::ostringstream oss;
-            oss << std::put_time(local_time, "%H:%M:%S");
-            
-            // Color
-            std::string begin_color = "";
-            std::string end_color = "";
-
-            if(mIsColored) {
-                std::array<std::string, 4> color_codes = {"", "", "\033[1;33m", "\033[1;31m"};
-                begin_color = color_codes[inLevel];
-                end_color = "\033[0m";
-            }
-
-            std::cout << begin_color << "[" << sLogLevelNames[inLevel] << "]" << end_color << " " << oss.str() << " - " << inText << std::endl;
+        if(mIsColored) {
+            std::array<std::string, 4> color_codes = {"", "", "\033[1;33m", "\033[1;31m"};
+            begin_color = color_codes[inLevel];
+            end_color = "\033[0m";
         }
+
+        std::cout << begin_color << "[" << sLogLevelNames[inLevel] << "]" << end_color << " " << getTimeString(inDate) << " - " << inText << std::endl;
     }
+}
 } // namespace phi
