35b2e766d9fe45d156a2a3aaf2f0495aca0e9693
Yann <yann.lemasson33@gmail.com>
2024-10-31 18:43:22 +0100

    Basic singleton

--- src/PhiLog.hpp
+++ src/PhiLog.hpp
@@ -1,2 +1,25 @@
 #pragma once
 
+namespace phi
+{
+    class PhiLog
+    {
+    private:
+        PhiLog(/* args */) {
+
+        }
+
+    public:
+        ~PhiLog() {
+            
+        }
+
+        static PhiLog &Get() {
+            static PhiLog me;
+            return me;
+        }
+    };
+} // namespace phi
+
+
+
