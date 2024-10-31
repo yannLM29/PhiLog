2068921c4a33d4db10f8bf644209c3a2b587dbb0
Yann <yann.lemasson33@gmail.com>
2024-10-31 22:54:05 +0100

    add comments to utilities

--- src/Utilities.hpp
+++ src/Utilities.hpp
@@ -1,3 +1,13 @@
+/**
+ * @file Utilities.hpp
+ * @author Yann Le Masson
+ * @brief Utilities static functions
+ * @version 0.1
+ * @date 2024-10-31
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
 #pragma once
 
 #include <string>
@@ -7,7 +17,13 @@
 
 namespace phi
 {
-
+    /**
+     * @brief Convert std::time_t to string following inFormat
+     * 
+     * @param inDate 
+     * @param inFormat Text format
+     * @return std::string 
+     */
     static std::string getTimeString(const std::time_t &inDate, const std::string &inFormat = "%H:%M:%S") {
         std::tm* local_time = std::localtime(&inDate);
         std::ostringstream oss;
